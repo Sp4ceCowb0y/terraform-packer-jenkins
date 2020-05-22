dbddbf82f414eca9c4bfba3d273065ec472d69e5
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2020-05-22 13:21:07 +0200

    changed some paths

--- scripts/jenkins-run-terraform.sh
+++ scripts/jenkins-run-terraform.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -ex
 AWS_REGION="eu-central-1"
-#cd terraform-course/jenkins-packer-demo
-cd terraform-packer-jenkins/
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
 sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
